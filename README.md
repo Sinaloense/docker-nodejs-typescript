29035c9553642807fc74d476dcaa226691da48bb
Sinaloense <sinaloensemel@gmail.com>
2024-11-28 18:42:23 -0600

    docker compose, archivos y requerimientos

---
+++ README.md
@@ -0,0 +1,10 @@
+# docker-nodejs-typescript
+
+## Requerimientos:
+- WSL (Windows): https://learn.microsoft.com/en-us/windows/wsl/install
+- Docker: https://docs.docker.com/get-docker
+- VSCode (Opcional): https://code.visualstudio.com/
+- VSCode Docker (Opcional): https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-docker
+
+## Basado en
+https://gist.github.com/Sinaloense/56fb49cedef6d73ff5fefeeab0fb3b56
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+// Define una interfaz para un usuario
+interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+const user: User = {
+    id: 1,
+    name: "Sina",
+    email: "sina@example.com"
+};
+
+function messageUser(user: User): string {
+    return `Hola, ${user.name}! Bienvenido a TypeScript.`;
+}
+
+function wait(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+let count = 0;
+
+while (true) {
+    count++;
+    console.log(`${messageUser(user)} ${count}`);
+    await wait(100);
+}
